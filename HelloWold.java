d9e2cd49e2f2f267819c42bab05dea826de1b33a
roro-zoro1 <Calvin.sites@malad.us>
2024-10-09 13:24:33 -0600

    Add files via upload

---
+++ HelloWold.java
@@ -0,0 +1,23 @@
+//writin by calvin sites
+//10/9/24
+//HelloWold for progreming class
+//
+public class HelloWold {
+
+    public static void main(String args[]) { //static method
+        String dadname="dina";
+        String momname="janea";
+        String sister1name="kaley";
+        String sister2name="riley";
+        String sister3name="alexys";
+        String brothname="brindin";
+        System.out.println("hellowold");
+        System.out.println("My name is calvin and I'm 14");
+        System.out.println("My dad's name is "+dadname);
+        System.out.println("My mom's name is "+momname);
+        System.out.println("My oldist sister's name is "+sister1name);
+        System.out.println("My other sister's name is "+sister2name);
+        System.out.println("My youngis sister's name is "+sister3name);
+        System.out.println("My brother's name is "+brothname);
+    }
+}
