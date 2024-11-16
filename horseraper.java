252a8157c6c5917095b8c08eb0bdbc7919ad8287
roro-zoro1 <Calvin.sites@malad.us>
2024-11-16 13:54:16 -0700

    Add files via upload

---
+++ horseraper.java
@@ -0,0 +1,14 @@
+//caratrd by Calvin Sites
+//it uses a bubble to count to 100 and than adds all the number's up
+//softwaredevelupment
+
+public class horseraper {
+    public static void main(String[] args){
+        Horse Noah=new Horse("Noah",2010);
+        Horse Horse1=new Horse("Mio",2011);
+        Horse Horse2=new Horse("Tion",2015);
+        System.out.println(Noah);
+        System.out.println(Horse1);
+        System.out.println(Horse2);
+        }
+    }
